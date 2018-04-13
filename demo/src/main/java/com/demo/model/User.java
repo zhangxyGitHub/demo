030292db9fe8e7025cc59c9c3589863aa6b067e0
zhangxiaoyong <zhangxiaoyong@bubi.cn>
2018-04-13 12:00:52 +0800

    创建项目

---
+++ demo/src/main/java/com/demo/model/User.java
@@ -0,0 +1,4 @@
+package com.demo.model;
+
+public class User {
+}
---
+++ demo/src/main/java/com/demo/service/impl/LoginServiceImpl.java
@@ -0,0 +1,6 @@
+package com.demo.service.impl;
+
+import com.demo.service.ILoginService;
+
+public class LoginServiceImpl implements ILoginService {
+}
